d261f2ae07b9b8c3274a185df28252eed4090be4
ady.to <adiyulianto61@gmail.com>
2023-03-12 16:16:40 +0700

    responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   const dataFooter = [
     {
-      title: 'about us',
+      title: 'about-us',
       desc1:
         'We have the confidence to provide the best service for you, with the support of Professional and Certified HR that we currently have and the high-quality materials we use and structured work techniques, we will be able to realize timely completion of work.',
     },
@@ -24,42 +24,57 @@ const Footer = () => {
       link: ['home', 'about-us', 'service', 'project'],
     },
   ];
-  console.log(dataFooter);
   return (
     <div className="bg-text-color-palette-2 text-text-color-palette-3 pt-10 px-4 xl:pt-11 2xl:pt-12">
       <div className="container mx-auto flex flex-col">
-        <div className="flex flex-row justify-between items-center">
-          <h1 className="uppercase font-Unlock xl:text-2xl 2xl:text-3xl">
+        <div className="flex justify-between mb-4 gap-y-1 lg:mb-0 items-center flex-col md:flex-row">
+          <h1 className="uppercase font-Unlock text-xl md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
             jhontraktor
           </h1>
-          <div className="flex flex-row items-center gap-x-3 xl:gap-x-4 2xl:gap-x-5">
+          <div className="flex flex-col md:flex-row items-center gap-x-3 md:text-sm lg:text-base xl:gap-x-4 2xl:gap-x-5">
             <h2>Subscribe To Our Newslatter:</h2>
             <div className="relative">
               <input
                 placeholder="Enter your email address"
                 type={'text'}
-                className="bg-transparent border-b-2 border-white/50 w-60 xl:w-60 2xl:w-72"
+                className="bg-transparent border-b-2 outline-none border-white/50 w-60 xl:w-60 2xl:w-72"
               />
               <FiArrowRight className="absolute  inset-y-0 right-0 my-auto" />
             </div>
           </div>
         </div>
-        <div className="flex flex-row justify-between 2xl:py-6">
+        <div className="flex flex-col space-y-3 md:space-y-4 lg:flex-row lg:justify-between lg:py-4 xl:py-5 2xl:py-6">
           {dataFooter.map((item) => (
-            <div className="flex flex-col">
-              <p className="capitalize font-medium 2xl:text-xl">{item.title}</p>
-              <p className="max-w-lg">{item.desc1}</p>
-              <p className="max-w-xs">
+            <div
+              className={`flex flex-col text-center lg:text-start ${
+                item.title === 'about-us'
+                  ? ' lg:w-80 xl:w-96 2xl:w-[500px]'
+                  : item.title === 'office'
+                  ? ' lg:w-60 xl:w-64 2xl:w-72'
+                  : item.title === 'contact'
+                  ? ' 2xl:w-64'
+                  : '2xl:w-20'
+              }`}
+            >
+              <p
+                className={`capitalize font-medium md:mb-2 lg:text-lg lg:mb-2 xl:text-xl xl:mb-2 2xl:text-xl 2xl:mb-3`}
+              >
+                {item.title === 'about-us' ? 'About Us' : item.title}
+              </p>
+              <p className=" text-sm max-w-lg mx-auto lg:text-base lg:w-full">
+                {item.desc1}
+              </p>
+              <p>
                 {item.address1}
                 <span className="block">{item.address2}</span>
               </p>
-              <p className="max-w-xs">
+              <p>
                 {item.email && `Email : ${item.email}`}
                 <span className="block">
                   {item.phone && `Phone : ${item.phone}`}
                 </span>
               </p>
-              <div className="flex flex-col gap-x-1">
+              <div className="flex flex-row  mx-auto gap-x-8 lg:flex-col lg:gap-y-1 lg:gap-x-0">
                 {item.link &&
                   item.link.map((value) => (
                     <Link to={`/${value}` === '/home' ? '/' : `/${value}`}>
@@ -69,9 +84,6 @@ const Footer = () => {
                     </Link>
                   ))}
               </div>
-              {/* {item.link.map((value) => (
-                <Link>{value}</Link>
-              ))} */}
             </div>
           ))}
         </div>
