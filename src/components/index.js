c4136e43818854497738c66bac1e361c6e10b63d
ady.to <adiyulianto61@gmail.com>
2023-03-13 15:09:24 +0700

    projectItem

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export { default as HomePage } from './HomePage';
 export { default as ExperienceItem } from './HomePage/ExperienceItem';
 export { default as HeroItem } from './HomePage/HeroItem';
-export { default as Projects } from './HomePage/HeroItem';
+export { default as Projects } from './Projects';
 export { default as StepItem } from './HomePage/StepItem';
 export { default as ClientCard } from './HomePage/ClientCard';
 export { default as MessageItem } from './HomePage/MessageItem';
